95059c38cc992046080dce606cef5ca14b169a32
Jose <kennrick@gmail.com>
2026-02-23 23:29:06 -0300

    update 23/02/2026 23:29

--- push.bat
+++ push.bat
@@ -7,29 +7,29 @@ echo.
 
 cd /d "%~dp0"
 
-echo [1/3] npm install no backend...
+echo [1/4] npm install no backend...
 cd backend
 call npm install
-if errorlevel 1 (
-    echo ERRO: npm install falhou!
-    pause
-    exit /b 1
-)
 cd ..
 
 echo.
-echo [2/3] git add + commit...
+echo [2/4] npm install no frontend...
+cd frontend
+call npm install
+cd ..
+
+echo.
+echo [3/4] git add + commit...
 git add .
 git commit -m "update %date% %time:~0,5%"
 
 echo.
-echo [3/3] git push...
+echo [4/4] git push...
 git push
 
 echo.
 echo ========================================
 echo   PUSH CONCLUIDO!
-echo   Aguarde o Railway fazer o deploy.
 echo ========================================
 echo.
 pause
